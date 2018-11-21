e262dc1629328c206a604f0fca4c38fb3c6f1859
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-11-21 16:37:09 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+5.times do
+  restaurant = Restaurant.new(name: Faker::Movies::StarWars.planet, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: "french")
+  restaurant.save!
+end
